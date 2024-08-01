000a8d9004b58e146be6f3c9d2193d6836fe8ff4
Chirag Kapil <your.email@example.com>
2024-08-01 23:16:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.body.style.backgroundColor='#ffffff';
+let text = document.getElementById('text');
+text.style.color='black';
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleSwitch = document.getElementById("mode-toggle");
+    toggleSwitch.addEventListener("change", () => {
+        if (toggleSwitch.checked) {
+            document.body.style.backgroundColor = "#000";
+            document.body.style.color = "#fff";
+            text.style.color='white';
+        } else {
+            document.body.style.backgroundColor = "#fff";
+            document.body.style.color = "#000";
+            text.style.color='black';
+        }
+    });
+});
