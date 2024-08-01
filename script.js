ed19ed7c70ff812e8c8937a3927efabf20269ac6
Chirag Kapil <your.email@example.com>
2024-08-01 23:42:18 +0530

    final

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.body.style.backgroundColor='#ffffff';
 let text = document.getElementById('text');
-let summerImg = document.getElementById("summer");
-let nightImg = document.getElementById('night');
 text.style.color='black';
 document.addEventListener("DOMContentLoaded", () => {
     const toggleSwitch = document.getElementById("mode-toggle");
@@ -9,15 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
         if (toggleSwitch.checked) {
             document.body.style.backgroundColor = "#000";
             document.body.style.color = "#fff";
-            summerImg.style.display='none';
-            nightImg.style.display='block';
             text.style.color='white';
         } else {
             document.body.style.backgroundColor = "#fff";
             document.body.style.color = "#000";
-            text.style.color='black';
-            summerImg.style.display='block';
-            nightImg.style.display='none';
+            text.style.color= 'black';
         }
     });
 });
