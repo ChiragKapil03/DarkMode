8b367ab724cc9c72f04b8d1e6fdb61ca36fc7aba
Chirag Kapil <your.email@example.com>
2024-08-01 23:33:59 +0530

    comitting finally

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.body.style.backgroundColor='#ffffff';
 let text = document.getElementById('text');
+let summerImg = document.getElementById("summer");
+let nightImg = document.getElementById('night');
 text.style.color='black';
 document.addEventListener("DOMContentLoaded", () => {
     const toggleSwitch = document.getElementById("mode-toggle");
@@ -7,11 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
         if (toggleSwitch.checked) {
             document.body.style.backgroundColor = "#000";
             document.body.style.color = "#fff";
+            summerImg.style.display='none';
+            nightImg.style.display='block';
             text.style.color='white';
         } else {
             document.body.style.backgroundColor = "#fff";
             document.body.style.color = "#000";
             text.style.color='black';
+            summerImg.style.display='block';
+            nightImg.style.display='none';
         }
     });
 });
